d63e306cd8879b24fd49a30e921fbfed28f2b2ae
Arsalan Raza Azmi <arsalan.azmi92@gmail.com>
2022-07-19 19:59:46 +0500

    changed product routes & added review route

--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -5,21 +5,32 @@ const {
   updateProduct,
   deleteProduct,
   getProductDetails,
+  createProductReview,
+  getProductReviews,
+  deleteReview,
 } = require("../controllers/productController");
-const { isAuthenticated, authorizeRoles } = require("../middleware/auth");
+const { isAuthenticatedUser, authorizeRoles } = require("../middleware/auth");
 
 const router = express.Router();
 
 router.route("/products").get(getAllProducts);
 
 router
-  .route("/product/new")
-  .post(isAuthenticated, authorizeRoles("admin"), createProduct);
+  .route("/admin/product/new")
+  .post(isAuthenticatedUser, authorizeRoles("admin"), createProduct);
 
 router
-  .route("/product/:id")
-  .put(isAuthenticated, authorizeRoles("admin"), updateProduct)
-  .delete(isAuthenticated, authorizeRoles("admin"), deleteProduct)
-  .get(getProductDetails);
+  .route("/admin/product/:id")
+  .put(isAuthenticatedUser, authorizeRoles("admin"), updateProduct)
+  .delete(isAuthenticatedUser, authorizeRoles("admin"), deleteProduct);
+
+router.route("/product/:id").get(getProductDetails);
+
+router.route("/review").put(isAuthenticatedUser, createProductReview);
+
+router
+  .route("/reviews")
+  .get(getProductReviews)
+  .delete(isAuthenticatedUser, deleteReview);
 
 module.exports = router;

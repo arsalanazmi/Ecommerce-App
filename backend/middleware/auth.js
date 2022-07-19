f6caccdbe8b76e731008c3be617c78051ff2bfd0
Arsalan Raza Azmi <arsalan.azmi92@gmail.com>
2022-07-19 20:01:49 +0500

    module name modified

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -3,7 +3,7 @@ const catchAsyncErrors = require("./catchAsyncErrors");
 const jwt = require("jsonwebtoken");
 const User = require("../models/userModel");
 
-exports.isAuthenticated = catchAsyncErrors(async (req, res, next) => {
+exports.isAuthenticatedUser = catchAsyncErrors(async (req, res, next) => {
   const { token } = req.cookies;
   console.log(token);
 

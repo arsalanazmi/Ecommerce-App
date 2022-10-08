c0727fb51fa7590bd7b00176c63f6628d61e93f9
Arsalan Raza Azmi <arsalan.azmi92@gmail.com>
2022-10-08 15:49:01 +0500

    Component Routes

---
+++ frontend/src/Routes.js
@@ -0,0 +1,10 @@
+import Header from "./component/layout/Header/Header.js";
+import Footer from "./component/layout/Footer/Footer.js";
+
+import Home from "./component/Home/Home.js";
+
+import ProductDetails from "./component/Product/ProductDetails.js";
+import Products from "./component/Product/Products.js";
+import Search from "./component/Product/Search.js";
+
+export { Header, Footer, Home, ProductDetails, Products, Search };

44fe37610c3775f8d30c0eff42af049840f985b8
Arsalan Raza Azmi <arsalan.azmi92@gmail.com>
2022-10-08 16:58:14 +0500

    Product Search

---
+++ frontend/src/component/Product/Search.js
@@ -0,0 +1,35 @@
+import React, { Fragment, useState } from "react";
+import "./Search.css";
+import history from "../../history"
+import MetaData from "../layout/MetaData";
+
+const Search = () => {
+  const [keyword, setKeyword] = useState("");
+  console.log(keyword);
+  
+  const searchSubmitHandler = (e) => {
+    e.preventDefault();
+    if (keyword.trim()) {
+      history.push(`/products/${keyword}`);
+      window.location.reload();
+    } else {
+      history.push("/products");
+    }
+  };
+
+  return (
+    <Fragment>
+      <MetaData title="Search A Product -- ECOMMERCE"/>
+      <form className="searchBox" onSubmit={searchSubmitHandler}>
+        <input
+          type="text"
+          placeholder="Search a Product..."
+          onChange={(e) => setKeyword(e.target.value)}
+        />
+        <input type="submit" value="Search" />
+      </form>
+    </Fragment>
+  );
+};
+
+export default Search;

bc7f9586f14c780b6ce147f4f2e1b87dcf80bec6
Arsalan Raza Azmi <arsalan.azmi92@gmail.com>
2022-10-20 20:10:57 +0500

    Cart

---
+++ frontend/src/component/Cart/Cart.js
@@ -0,0 +1,105 @@
+import React, { Fragment } from "react";
+import "./Cart.css";
+import CartItemCard from "./CartItemCard.js";
+import { useSelector, useDispatch } from "react-redux";
+import { addItemsToCart, removeItemsFromCart } from "../../actions/cartActions";
+import Typography from "@mui/material/Typography";
+import RemoveShoppingCartIcon from "@mui/icons-material/RemoveShoppingCart";
+import { Link } from "react-router-dom";
+
+const Cart = () => {
+  const dispatch = useDispatch();
+  const { cartItems } = useSelector((state) => state.cart);
+
+  const increaseQuantity = (id, quantity, stock) => {
+    const newQty = quantity + 1;
+
+    if (stock <= quantity) {
+      return;
+    }
+    dispatch(addItemsToCart(id, newQty));
+  };
+
+  const deccreaseQuantity = (id, quantity) => {
+    const newQty = quantity - 1;
+
+    if (1 >= quantity) {
+      return;
+    }
+    dispatch(addItemsToCart(id, newQty));
+  };
+
+  const deleteCartItems = (id) => {
+    dispatch(removeItemsFromCart(id));
+  };
+  
+  return (
+    <Fragment>
+      {cartItems.length === 0 ? (
+        <div className="emptyCart">
+          <RemoveShoppingCartIcon />
+          <Typography>No Product in Your Cart</Typography>
+          <Link to="/products">View Products</Link>
+        </div>
+      ) : (
+        <Fragment>
+          <div className="cartPage">
+            <div className="cartHeader">
+              <p>Product</p>
+              <p>Quantity</p>
+              <p>Subtotal</p>
+            </div>
+
+            {cartItems &&
+              cartItems.map((item) => (
+                <div key={item.product} className="cartContainer">
+                  <CartItemCard item={item} deleteCartItems={deleteCartItems} />
+                  <div className="cartInput">
+                    <button
+                      onClick={() =>
+                        deccreaseQuantity(item.product, item.quantity)
+                      }
+                    >
+                      -
+                    </button>
+                    <input readOnly type="number" value={item.quantity} />
+                    <button
+                      onClick={() =>
+                        increaseQuantity(
+                          item.product,
+                          item.quantity,
+                          item.stock
+                        )
+                      }
+                    >
+                      +
+                    </button>
+                  </div>
+                  <p className="cartSubTotal">{`PKR ${
+                    item.price * item.quantity
+                  }`}</p>
+                </div>
+              ))}
+
+            <div className="cartGrossTotal">
+              <div></div>
+              <div className="cartGrossTotalBox">
+                <p>Gross Total</p>
+                <p>{`PKR ${cartItems.reduce(
+                  (acc, item) => acc + item.quantity * item.price,
+                  0
+                )}`}</p>
+              </div>
+              <div></div>
+              <div className="checkOutBtn">
+                <button>Check Out</button>
+              </div>
+            </div>
+          </div>
+        </Fragment>
+      )}
+    </Fragment>
+  );
+};
+
+export default Cart;

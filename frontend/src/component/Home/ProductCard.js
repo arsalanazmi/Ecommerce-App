d2dc51cd63378db0d298c0cc0b45b32f6a9c5a64
Arsalan Raza Azmi <arsalan.azmi92@gmail.com>
2022-11-01 23:02:28 +0500

    used Rating in place ReactStars

--- frontend/src/component/Home/ProductCard.js
+++ frontend/src/component/Home/ProductCard.js
@@ -1,28 +1,25 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import ReactStars from "react-rating-stars-component";
+import Rating from "@mui/material/Rating";
 
 const ProductCard = ({ product }) => {
   const options = {
-    edit: false,
-    color: "rgba(20,20,20,0.1)",
-    activeColor: "tomato",
-    size: window.innerWidth < 600 ? 18 : 25,
-    value: product.ratings,
-    isHalf: true,
+    value: product?.ratings,
+    precision: 0.5,
+    readOnly: true,
   };
   
   return (
-    <Link className="productCard" to={`/product/${product._id}`}>
-      <img src={product.images[0].url} alt={product.name} />
-      <p>{product.name}</p>
+    <Link className="productCard" to={`/product/${product?._id}`}>
+      <img src={product?.images[0].url} alt={product?.name} />
+      <p>{product?.name}</p>
 
       <div>
-        <ReactStars {...options} />
-        <span className="productCardSpan">{product.numOfReviews} Reviews</span>
+        <Rating {...options} />
+        <span className="productCardSpan">{product?.numOfReviews} Reviews</span>
       </div>
 
-      <span>{`PKR${product.price}`}</span>
+      <span>{`PKR${product?.price}`}</span>
     </Link>
   );
 };

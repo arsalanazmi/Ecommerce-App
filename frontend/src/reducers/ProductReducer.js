cc504e75ec2a62af4d152d192f2ab5952e8b55dd
Arsalan Raza Azmi <arsalan.azmi92@gmail.com>
2022-11-01 22:43:46 +0500

    added newReviewReducer

--- frontend/src/reducers/ProductReducer.js
+++ frontend/src/reducers/ProductReducer.js
@@ -5,6 +5,10 @@ import {
   PRODUCT_DETAILS_REQUEST,
   PRODUCT_DETAILS_SUCCESS,
   PRODUCT_DETAILS_FAIL,
+  NEW_REVIEW_REQUEST,
+  NEW_REVIEW_SUCCESS,
+  NEW_REVIEW_RESET,
+  NEW_REVIEW_FAIL,
   CLEAR_ERRORS,
 } from "../constants/productConstant";
 
@@ -15,6 +19,7 @@ export const productReducer = (state = { products: [] }, action) => {
         loading: true,
         products: [],
       };
+
     case ALL_PRODUCT_SUCCESS:
       return {
         loading: false,
@@ -22,16 +27,19 @@ export const productReducer = (state = { products: [] }, action) => {
         productsCount: action.payload.productsCount,
         resultPerPage: action.payload.resultPerPage,
       };
+
     case ALL_PRODUCT_FAIL:
       return {
         loading: false,
         error: action.payload,
       };
+
     case CLEAR_ERRORS:
       return {
         ...state,
         error: null,
       };
+
     default:
       return state;
   }
@@ -44,21 +52,63 @@ export const productDetailsReducer = (state = { product: {} }, action) => {
         loading: true,
         ...state,
       };
+
     case PRODUCT_DETAILS_SUCCESS:
       return {
         loading: false,
         product: action.payload,
       };
+
     case PRODUCT_DETAILS_FAIL:
       return {
         loading: false,
         error: action.payload,
       };
+
     case CLEAR_ERRORS:
       return {
         ...state,
         error: null,
       };
+
+    default:
+      return state;
+  }
+};
+
+export const newReviewReducer = (state = {}, action) => {
+  switch (action.type) {
+    case NEW_REVIEW_REQUEST:
+      return {
+        ...state,
+        loading: true,
+      };
+
+    case NEW_REVIEW_SUCCESS:
+      return {
+        loading: false,
+        success: action.payload,
+      };
+
+    case NEW_REVIEW_FAIL:
+      return {
+        ...state,
+        loading: false,
+        error: action.payload,
+      };
+
+    case NEW_REVIEW_RESET:
+      return {
+        ...state,
+        success: false,
+      };
+
+    case CLEAR_ERRORS:
+      return {
+        ...state,
+        error: null,
+      };
+
     default:
       return state;
   }
